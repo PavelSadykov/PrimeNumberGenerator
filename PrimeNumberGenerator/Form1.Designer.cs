f29747b435d53f028bcde444e74b99710909c3c7
PavelSadykov <2439747@gmail.com>
2023-12-17 10:36:42 +0500

    Добавьте файлы проекта.

---
+++ PrimeNumberGenerator/Form1.Designer.cs
@@ -0,0 +1,130 @@
+﻿namespace PrimeNumberGenerator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.startButton = new System.Windows.Forms.Button();
+            this.listBoxPrimes = new System.Windows.Forms.ListBox();
+            this.txtStart = new System.Windows.Forms.TextBox();
+            this.txtEnd = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // startButton
+            // 
+            this.startButton.Location = new System.Drawing.Point(446, 80);
+            this.startButton.Name = "startButton";
+            this.startButton.Size = new System.Drawing.Size(120, 44);
+            this.startButton.TabIndex = 0;
+            this.startButton.Text = "Start";
+            this.startButton.UseVisualStyleBackColor = true;
+            this.startButton.Click += new System.EventHandler(this.startButton_Click_1);
+            // 
+            // listBoxPrimes
+            // 
+            this.listBoxPrimes.FormattingEnabled = true;
+            this.listBoxPrimes.Location = new System.Drawing.Point(266, 80);
+            this.listBoxPrimes.Name = "listBoxPrimes";
+            this.listBoxPrimes.Size = new System.Drawing.Size(174, 329);
+            this.listBoxPrimes.TabIndex = 1;
+            // 
+            // txtStart
+            // 
+            this.txtStart.Location = new System.Drawing.Point(81, 80);
+            this.txtStart.Name = "txtStart";
+            this.txtStart.Size = new System.Drawing.Size(141, 20);
+            this.txtStart.TabIndex = 2;
+            // 
+            // txtEnd
+            // 
+            this.txtEnd.Location = new System.Drawing.Point(81, 146);
+            this.txtEnd.Name = "txtEnd";
+            this.txtEnd.Size = new System.Drawing.Size(141, 20);
+            this.txtEnd.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(78, 64);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(144, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Введите начало диапазона";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(78, 130);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(139, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Введите конец диапазона";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(263, 64);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(84, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Простые числа";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtEnd);
+            this.Controls.Add(this.txtStart);
+            this.Controls.Add(this.listBoxPrimes);
+            this.Controls.Add(this.startButton);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button startButton;
+        private System.Windows.Forms.ListBox listBoxPrimes;
+        private System.Windows.Forms.TextBox txtStart;
+        private System.Windows.Forms.TextBox txtEnd;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ PrimeNumberGenerator/Form1.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PrimeNumberGenerator
+{
+    public partial class Form1 : Form
+    {
+        private Thread primeThread;
+        private bool isGenerating;//лог переменная работы потока
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        //метод генерации простых чисел
+
+        private void GeneratePrimeNumbers(int start, int end)
+        {
+            for (int i = start; i <= end && isGenerating; i++) // перебираем числа внутри диапазона (если  лог переменная isGenerating true )
+            {
+                if (IsPrime(i)) // отправляем в метод проверки
+                {
+                    this.Invoke((MethodInvoker)delegate
+                    {
+                        listBoxPrimes.Items.Add(i);// простое число добавляем  в лист бокс
+                    });
+                }
+            }
+
+            isGenerating = false;
+            this.Invoke((MethodInvoker)delegate
+            {
+                startButton.Text = "Start";
+            });
+        }
+        //метод проверки числа на простое 
+        private bool IsPrime(int number)
+        {
+            if (number <= 1)
+                return false;
+
+            for (int i = 2; i <= Math.Sqrt(number); i++)// метод проверки деления на все числа от 2 до квадр корня из числа
+            {
+                if (number % i == 0)
+                    return false;
+            }
+
+            return true;
+        }
+
+
+        [Obsolete]
+        private void startButton_Click_1(object sender, EventArgs e)
+        {
+            if (!isGenerating)
+            {// если процесс не запущен, метод получает нач и конечное значения и начинает генерацию в потоке
+                int startNumber = string.IsNullOrEmpty(txtStart.Text) ? 2 : int.Parse(txtStart.Text);
+                int endNumber = string.IsNullOrEmpty(txtEnd.Text) ? int.MaxValue : int.Parse(txtEnd.Text);
+
+                isGenerating = true;
+                primeThread = new Thread(() => GeneratePrimeNumbers(startNumber, endNumber));
+                primeThread.Start();
+                startButton.Text = "Stop";
+            }
+            else
+            {//иначе останавливает процесс
+                isGenerating = false;
+                startButton.Text = "Start";
+                primeThread.Suspend(); 
+            }
+        }
+
+       
+        [Obsolete]
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {// обработчик события на случай закрытия формы. 
+            {
+                isGenerating = false;
+                primeThread.Suspend();
+            }
+        }
+    }
+}
